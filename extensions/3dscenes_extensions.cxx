38aac125813b274ceb52ef6a7c26a69c04a0178e
midori <cleoold@gmail.com>
2022-05-10 00:08:04 -0400

    set up cpp and lua and dummy webpage

---
+++ extensions/3dscenes_extensions.cxx
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "emscripten.h"
+
+extern "C"
+{
+    #include "lua.h"
+    #include "lauxlib.h"
+    #include "lualib.h"
+}
+
+extern "C"
+{
+
+EMSCRIPTEN_KEEPALIVE
+void execute_lua(char const *code)
+{
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+
+    auto error = luaL_loadstring(L, code) || lua_pcall(L, 0, 0, 0);
+    if (error) {
+        fprintf(stderr, "%s\n", lua_tostring(L, -1));
+        lua_pop(L, 1); /* pop error message from the stack */
+    }
+
+    lua_close(L);
+}
+
+}
